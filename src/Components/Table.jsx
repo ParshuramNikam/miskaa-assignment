b48e06cbf4b0345b446e1123f55e6d3bfcdc8aeb
Parshuram Nikam <parshunikam0612@gmail.com>
2021-09-14 16:24:00 +0530

    'initial'

---
+++ src/Components/Table.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Thead from './Thead';
+
+const Table = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState('');
+
+    const setButton = () => {
+        setLoading('Loading...');
+        setTimeout(() => {
+            setLoading('Refresh');
+        }, 500);
+    }
+    const getAllData = () => {
+        setData([]);
+        fetch('https://restcountries.eu/rest/v2/region/asia')
+            .then((res) => res.json())
+            .then((countryData) => setData(countryData))
+            .catch((err) => console.log(err.message));
+        setButton();
+    }
+
+    useEffect(() => {
+        getAllData();
+    }, []);
+
+    return (
+        <>
+            <div className="center">
+                <button onClick={(e) => getAllData()}> {loading} </button>
+            </div>
+            <div className="info">
+                <table className="sticky" cellSpacing="0" cellPadding="10">
+                    {data.length ? <Thead /> : ""}
+                    <tbody>
+                        {
+                            data.map((country, index) => {
+                                return (
+                                    <tr key={country.name}>
+                                        <td>{++index}</td>
+                                        <td>{country.name}</td>
+                                        <td>{country.capital}</td>
+                                        <td><img src={country.flag} alt="falg" width="35" /></td>
+                                        <td>{country.subregion}</td>
+                                        <td>{country.population}</td>
+                                        <td>
+                                            <ol>{
+                                                country.borders.map((border) => {
+                                                    return (
+                                                        <li key={border}>{border}</li>
+                                                    )
+                                                })
+                                            }</ol>
+                                        </td>
+                                        <td>
+                                            <ol>{
+                                                country.languages.map((language) => {
+                                                    return (
+                                                        <li key={language.name}>{language.name} ({language.nativeName})</li>
+                                                    )
+                                                })
+                                            }</ol>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+}
+
+export default Table
---
+++ src/Components/Thead.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Thead = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Sr.No</th>
+                <th>Name</th>
+                <th>Capital</th>
+                <th>Flag</th>
+                <th>Subregion</th>
+                <th>Population</th>
+                <th>Border</th>
+                <th>Language</th>
+            </tr>
+        </thead>
+    )
+}
+
+export default Thead;
